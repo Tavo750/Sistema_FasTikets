a4ebc0507382828d597ced80cd6f18356dc8b7fc
Tavo750 <a20215847@pucp.edu.pe>
2025-10-02 21:57:01 -0500

    creacion de home

--- plantilla-frontend-angular/src/app/app-routing.module.ts
+++ plantilla-frontend-angular/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { LayoutComponent } from './core/components/layout/layout.component';
 import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path: 'lote', component: LayoutComponent, canActivate: [AuthGuard], children: [
       { path: '', loadChildren: () => import('./pages/modules/lote/lote.module').then(m => m.LoteModule) },
@@ -32,6 +32,7 @@ const routes: Routes = [
   },
   { path: 'login', loadChildren: () => import('./pages/full-pages/login/login.module').then(m => m.LoginModule) },
   { path: 'error', loadChildren: () => import('./pages/full-pages/error/error.module').then(m => m.ErrorModule) },
+  { path: 'home', loadChildren: () => import('./pages/full-pages/home/home.module').then(m => m.HomeModule) },
   { path: 'crear_usuario', loadChildren: () => import('./pages/full-pages/crear-usuario/crear-usuario.module').then(m => m.CrearUsuarioModule) },
   { path: 'cambiar_contra', loadChildren: () => import('./pages/full-pages/cambiar-contra/cambiar-contra.module').then(m => m.CambiarContraModule) },
   { path: '**', redirectTo: 'error' },
