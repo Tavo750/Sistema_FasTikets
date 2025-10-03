91450e51d9e4213c7c790a1b82ef50a5e9716a9b
gustavo_sanchez <a20215847@pucp.edu.com.pe>
2025-10-03 18:42:11 -0500

    ultimno

--- plantilla-frontend-angular/src/app/app-routing.module.ts
+++ plantilla-frontend-angular/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './core/components/layout/layout.component';
 import { AuthGuard } from './core/guards/auth.guard';
+import { HomeComponent } from './pages/full-pages/home - eliminar/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
@@ -10,11 +11,11 @@ const routes: Routes = [
       { path: '', loadChildren: () => import('./pages/modules/lote/lote.module').then(m => m.LoteModule) },
     ],
   },
-  {
-    path: 'logo', component: LayoutComponent, canActivate: [AuthGuard], children: [
-      { path: '', loadChildren: () => import('./pages/modules/logo/logo.module').then(m => m.LogoModule) },
-    ]
-  },
+  // {
+  //   path: 'logo', component: LayoutComponent, canActivate: [AuthGuard], children: [
+  //     { path: '', loadChildren: () => import('./pages/modules/logo/logo.module').then(m => m.LogoModule) },
+  //   ]
+  // },
   {
     path: 'facturacion', component: LayoutComponent, canActivate: [AuthGuard], children: [
       { path: '', loadChildren: () => import('./pages/modules/facturacion/facturacion.module').then(m => m.FacturacionModule) },
@@ -32,18 +33,18 @@ const routes: Routes = [
   },
 
   {
-    path: 'carrito', component: LayoutComponent, canActivate: [AuthGuard], children: [
+    path: 'logo', component: HomeComponent, canActivate: [AuthGuard], children: [
       { path: '', loadChildren: () => import('./pages/modules/cotizacion/cotizacion.module').then(m => m.CotizacionModule) },
     ]
   },
 
 
 
-
+  { path: 'principal', loadChildren: () => import('./pages/modules/principal/principal.module').then(m => m.PrincipalModule) },
 
   { path: 'login', loadChildren: () => import('./pages/full-pages/login/login.module').then(m => m.LoginModule) },
   { path: 'error', loadChildren: () => import('./pages/full-pages/error/error.module').then(m => m.ErrorModule) },
-  { path: 'home', loadChildren: () => import('./pages/full-pages/home/home.module').then(m => m.HomeModule) },
+  // { path: 'home', loadChildren: () => import('./pages/full-pages/home/home.module').then(m => m.HomeModule) },
   { path: 'crear_usuario', loadChildren: () => import('./pages/full-pages/crear-usuario/crear-usuario.module').then(m => m.CrearUsuarioModule) },
   { path: 'cambiar_contra', loadChildren: () => import('./pages/full-pages/cambiar-contra/cambiar-contra.module').then(m => m.CambiarContraModule) },
   { path: '**', redirectTo: 'error' },
