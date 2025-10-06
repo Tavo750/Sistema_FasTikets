0d7e77e446a67f7a2a4f1df2d4aa1a6472ce2fa8
gustavo_sanchez <a20215847@pucp.edu.com.pe>
2025-10-06 10:58:55 -0500

    estructura usuario

---
+++ plantilla-frontend-angular/src/app/pages/modules/usuario/components/perfil-personal/perfil-personal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil-personal',
+  standalone: false,
+  templateUrl: './perfil-personal.component.html',
+  styleUrl: './perfil-personal.component.css'
+})
+export class PerfilPersonalComponent {
+
+}
