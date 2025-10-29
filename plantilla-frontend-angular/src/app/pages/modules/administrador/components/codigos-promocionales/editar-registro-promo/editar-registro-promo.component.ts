e322b41c2f4134948e44e4fb16af8c774b60b47d
joharRC7 <164355950+joharRC7@users.noreply.github.com>
2025-10-29 12:17:13 -0500

    agregacion de modulos de editar codigos promocionales

--- plantilla-frontend-angular/src/app/pages/modules/administrador/components/codigos-promocionales/editar-registro-promo/editar-registro-promo.component.ts
+++ plantilla-frontend-angular/src/app/pages/modules/administrador/components/codigos-promocionales/editar-registro-promo/editar-registro-promo.component.ts
@@ -1,11 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+
 
 @Component({
   selector: 'app-editar-registro-promo',
   standalone: false,
   templateUrl: './editar-registro-promo.component.html',
-  styleUrl: './editar-registro-promo.component.css'
+  styleUrls: ['./editar-registro-promo.component.css'],
+  providers: [MessageService]
 })
-export class EditarRegistroPromoComponent {
+export class EditarRegistroPromoComponent implements OnInit  {
+   tipos = [
+    { label: 'Porcentaje', value: 'PORCENTAJE' },
+    { label: 'Dinero',     value: 'DINERO' }
+  ];
+  segmentos = [
+    { label: 'Evento',  value: 'EVENTO' },
+    { label: 'Cliente', value: 'CLIENTE' }
+  ];
+  elementosOptions = [
+    { label: 'Concierto Shakira', value: 'Concierto Shakira' },
+    { label: 'Concierto Nuevo',   value: 'Concierto Nuevo' },
+    { label: 'Estudiante', value: 'Estudiante' },
+    { label: 'VIP', value: 'VIP' },
+    { label: 'General', value: 'General' },
+  ];
 
+  form!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private toast: MessageService
+  ) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      codigo: ['', Validators.required],
+      descripcion: [''],
+      fechaExpiracion: [null, Validators.required],
+      tipo: [null, Validators.required],
+      valor: [null, Validators.required],
+      segmento: [null, Validators.required],
+      elementos: [[], Validators.required],
+    });
+
+    const id = this.route.snapshot.paramMap.get('id');
+
+    // Simulación de carga de datos para edición
+    if (id) {
+      this.form.patchValue({
+        codigo: 'SHAKIRACNRT',
+        descripcion: 'Código promocional concierto',
+        fechaExpiracion: new Date(), // demo
+        tipo: 'PORCENTAJE',
+        valor: 10,
+        segmento: 'EVENTO',
+        elementos: ['Concierto Shakira', 'Concierto Nuevo']
+      });
+    }
+  }
+
+  guardar(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    this.toast.add({
+      severity: 'success',
+      summary: 'Actualizado',
+      detail: 'Cambios guardados'
+    });
+
+    this.router.navigate(['/administrador/codigosPromocionales']);
+  }
 }
