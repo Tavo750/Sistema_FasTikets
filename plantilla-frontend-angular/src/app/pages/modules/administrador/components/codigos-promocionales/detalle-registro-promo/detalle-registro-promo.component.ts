2797447a695e15f243cd4cd71d1a1fe830450d97
joharRC7 <164355950+joharRC7@users.noreply.github.com>
2025-10-29 12:10:21 -0500

    agregue detalle y regsitrar promo

--- plantilla-frontend-angular/src/app/pages/modules/administrador/components/codigos-promocionales/detalle-registro-promo/detalle-registro-promo.component.ts
+++ plantilla-frontend-angular/src/app/pages/modules/administrador/components/codigos-promocionales/detalle-registro-promo/detalle-registro-promo.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-detalle-registro-promo',
   standalone: false,
   templateUrl: './detalle-registro-promo.component.html',
-  styleUrl: './detalle-registro-promo.component.css'
+  styleUrls: ['./detalle-registro-promo.component.css']
 })
-export class DetalleRegistroPromoComponent {
+export class DetalleRegistroPromoComponent implements OnInit{
+  data = {
+    codigo: 'SHAKIRACNRT',
+    descripcion: 'Código promocional concierto',
+    fechaExpiracion: '10/09/2025',
+    tipo: 'PORCENTAJE', // PORCENTAJE | DINERO
+    valor: 10,
+    segmento: 'EVENTO',  // EVENTO | CLIENTE
+    elementos: ['Concierto Shakira', 'Concierto Nuevo']
+  };
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    // TODO: cargar detalle por id
+  }
 }
