889f75443a6fa5fc3b02150703804b1e62522403
Tavo750 <a20215847@pucp.edu.pe>
2025-11-16 17:11:46 -0500

    interface cambio-conta

---
+++ plantilla-frontend-angular/src/app/core/interfaces/cambio-contra.interface.ts
@@ -0,0 +1,18 @@
+export interface OlvidoContraResponse {
+    ok:      boolean;
+    mensaje: string;
+    data:    string;
+}
+
+
+export interface ValidaCodigoResponse {
+    ok:      boolean;
+    mensaje: string;
+    data:    string;
+}
+
+export interface ResetResponse {
+    ok:      boolean;
+    mensaje: string;
+    data:    string;
+}
