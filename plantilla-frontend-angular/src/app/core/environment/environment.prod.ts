a2aa24970e5744757c5db45fa9ac2582dbe9f226
Tavo750 <a20215847@pucp.edu.pe>
2025-11-18 10:51:34 -0500

    actualizacion baseUrl

--- plantilla-frontend-angular/src/app/core/environment/environment.prod.ts
+++ plantilla-frontend-angular/src/app/core/environment/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   useMockAuth: false, // En producción usar auth real
-  baseUrl: 'https://8d8f0c0cc106.ngrok-free.app/api/v1' // Backend en Elastic Beanstalk
+  baseUrl: 'https://api.francoricra.online/api/v1' // Backend en Elastic Beanstalk
 };
--- plantilla-frontend-angular/src/app/core/environment/environment.ts
+++ plantilla-frontend-angular/src/app/core/environment/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   useMockAuth: true, // en local es true
   // useMockAuth: false // en produccion es false
-  baseUrl: 'https://8d8f0c0cc106.ngrok-free.app/api/v1'
+  baseUrl: 'https://api.francoricra.online/api/v1'
 };
