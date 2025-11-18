4a3133f8fbef4ade4077a49081a274120026fb5f
Luffystyle <torres.d.miguel20@gmail.com>
2025-11-17 21:27:56 -0500

    modificacion

--- plantilla-frontend-angular/src/app/global.ts
+++ plantilla-frontend-angular/src/app/global.ts
@@ -4,9 +4,9 @@ export var version: string = `Versión 1.0.0`;
 export var titulo: string = `Plantilla`;
 export var tituloVersion: string = `v(1.0.0)`;
 
+// URL de producción para AWS
+export var baseUrl = 'http://fasticket-alb-157500411.us-east-1.elb.amazonaws.com/api/v1';
 
-//export var url: string = 'http://fasticket-alb-prod-23788338.us-east-1.elb.amazonaws.com';
 export var anio: number = 2025;
 
-export var tiempoInactividadAlarma: number = 8 * (60 * 60 * 1000); //X (unidades horas)
-export var baseUrl = 'http://fasticket-alb-157500411.us-east-1.elb.amazonaws.com/api/v1';
+export var tiempoInactividadAlarma: number = 8 * (60 * 60 * 1000);
