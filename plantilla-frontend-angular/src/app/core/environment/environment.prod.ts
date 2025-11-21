360b7aba3b13a389cf9aba3a51b78622626e9941
Tavo750 <a20215847@pucp.edu.pe>
2025-11-21 12:24:45 -0500

    prueba

--- plantilla-frontend-angular/src/app/core/environment/environment.prod.ts
+++ plantilla-frontend-angular/src/app/core/environment/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   useMockAuth: false, // En producción usar auth real
-  baseUrl: 'http://fasticket-backend-spring-env.eba-pzm5ntbm.us-east-1.elasticbeanstalk.com' // Backend en Elastic Beanstalk
+  baseUrl: 'http://fasticket-backend-spring-env.eba-pzm5ntbm.us-east-1.elasticbeanstalk.com/api/v1' // Backend en Elastic Beanstalk
 };
