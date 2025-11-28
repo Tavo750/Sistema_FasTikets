3efdbf07755aef8369ff51d3c8fe5d2212273dcf
Tavo750 <a20215847@pucp.edu.pe>
2025-11-28 16:41:27 -0500

    cambio url back

--- plantilla-frontend-angular/src/app/global.ts
+++ plantilla-frontend-angular/src/app/global.ts
@@ -5,7 +5,7 @@ export var titulo: string = `Plantilla`;
 export var tituloVersion: string = `v(1.0.0)`;
 
 // URL del backend local
-export var baseUrl = 'http://localhost:8081/api/v1';
+export var baseUrl = 'http://fasticket-backend-env.eba-vav3jizi.us-east-1.elasticbeanstalk.com/api/v1';
 
 export var anio: number = 2025;
 
