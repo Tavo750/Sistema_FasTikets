808bdb2e1e0d822fff8a9caa936d9e0ba96570a3
Tavo750 <a20215847@pucp.edu.pe>
2025-11-30 04:18:03 -0500

    implementacion de boton para reenviar correo de verificacion

---
+++ plantilla-frontend-angular/src/app/core/interfaces/reenvio-verificacion.interface.ts
@@ -0,0 +1,5 @@
+export interface ReenvioVerificacionResponse {
+    ok:      boolean;
+    mensaje: string;
+    data:    string;
+}
--- plantilla-frontend-angular/src/app/core/services/registro-usuario.service.ts
+++ plantilla-frontend-angular/src/app/core/services/registro-usuario.service.ts
@@ -6,6 +6,7 @@ import { RegistroResponse, RegistroUsuario } from '../interfaces/registro_usuari
 import { Departamento, departamentoResponse, Distrito, distritoResponse, Provincia, provinciaResponse } from '../interfaces/ubigeo.interface';
 import { baseUrl } from '../../global';
 import { VerificacionCuentaResponse } from '../interfaces/verificacion-cuenta.interface';
+import { ReenvioVerificacionResponse } from '../interfaces/reenvio-verificacion.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -42,9 +43,9 @@ export class RegistroUsuarioService {
     return this.http.post<VerificacionCuentaResponse>(url, {token});
   }
 
-  postReenvioVerificacionCorreo(email: string): Observable<VerificacionCuentaResponse> {
+  postReenvioVerificacionCorreo(email: string): Observable<ReenvioVerificacionResponse> {
     const url = `${baseUrl}/auth/verificar/reenviar`;
-    return this.http.post<VerificacionCuentaResponse>(url, {email});
+    return this.http.post<ReenvioVerificacionResponse>(url, {email});
   }
 
 }
