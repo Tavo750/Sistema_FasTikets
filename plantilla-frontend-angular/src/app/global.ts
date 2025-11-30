48bce11d3bddb51591317c59d662239e33569187
Tavo750 <a20215847@pucp.edu.pe>
2025-11-30 16:06:54 -0500

    url

--- plantilla-frontend-angular/src/app/global.ts
+++ plantilla-frontend-angular/src/app/global.ts
@@ -5,7 +5,7 @@ export var titulo: string = `Plantilla`;
 export var tituloVersion: string = `v(1.0.0)`;
 
 // URL del backend
-export var baseUrl = 'http://localhost:8081/api/v1';
+export var baseUrl = 'https://api.francoricra.online/api/v1';
 
 export var anio: number = 2025;
 
